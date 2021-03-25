e191e11f0797c8cc71abe0d9971ae1b5fb605918
피수영 <vltndud123@naver.com>
2021-03-25 19:00:58 +0900

    First commit

---
+++ src/main/java/Bill/Gates/of/Inha/Application.java
@@ -0,0 +1,13 @@
+package Bill.Gates.of.Inha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/Bill/Gates/of/Inha/ApplicationTests.java
@@ -0,0 +1,13 @@
+package Bill.Gates.of.Inha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
