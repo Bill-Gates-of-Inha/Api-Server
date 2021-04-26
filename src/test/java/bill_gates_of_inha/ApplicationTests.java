ea0aeeecc57b855e4ca5dea096b39570f869ba83
sukstar76 <sukstar76@gmail.com>
2021-04-27 05:02:28 +0900

    fix:

--- src/test/java/bill_gates_of_inha/ApplicationTests.java
+++ src/test/java/bill_gates_of_inha/ApplicationTests.java
@@ -2,7 +2,9 @@ package bill_gates_of_inha;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles(value = "test")
 @SpringBootTest
 class ApplicationTests {
 
